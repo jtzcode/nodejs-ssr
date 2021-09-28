055b3e826b533107c78fa8e89a342a422a0e6cf4
大灰 <tianze@192.168.124.101>
2021-09-28 19:48:26 +0800

    learn ssr

---
+++ geek-details/client/client.js
@@ -0,0 +1,44 @@
+const EasySock = require('easy_sock');
+const protobuf = require('protocol-buffers');
+const fs = require('fs')
+
+const schemas = protobuf(fs.readFileSync(`${__dirname}/detail.proto`));
+
+const easySock = new EasySock({
+    ip: '127.0.0.1',
+    port: 4000,
+    timeout: 500,
+    keepAlive: true
+});
+
+easySock.encode = function(data , seq) {
+    const body = schemas.ColumnRequest.encode(data);
+    const head = Buffer.alloc(8);
+    head.writeInt32BE(seq);
+    head.writeInt32BE(body.length, 4);
+    return Buffer.concat([head, body]);
+};
+
+easySock.decode = function(buffer) {
+    const seq = buffer.readInt32BE();
+    const body = schemas.ColumnResponse.decode(buffer.slice(8));
+    return {
+        result: body,
+        seq
+    };
+};
+
+easySock.isReceiveComplete = function(buffer) {
+    if (buffer.length < 8) {
+        return 0;
+    }
+
+    const bodyLen = buffer.readInt32BE(4);
+    if (buffer.length >= bodyLen + 8) {
+        return bodyLen + 8;
+    } else {
+        return 0;
+    }
+};
+
+module.exports = easySock;
